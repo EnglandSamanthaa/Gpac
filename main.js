34734ed479dacb9fba518e331a9a96bfa13aa772
Samantha England <64668742+EnglandSamanthaa@users.noreply.github.com>
2023-11-01 11:32:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// function setBodyMarginTop() {
+//     var headerHeight = document.querySelector('body > header').offsetHeight;
+//     document.body.style.marginTop = headerHeight + 'px';
+// }
+
+// window.addEventListener('load', setBodyMarginTop);
+// window.addEventListener('resize', setBodyMarginTop);
